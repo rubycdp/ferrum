3d88a812fd8c86dcf3e180139ff8bc882f27ab2c
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2021-02-03 01:47:03 +0500

    implement Ferrum::RGBA class

---
+++ spec/rbga_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "ferrum/rbga"
+
+module Ferrum
+  describe RGBA do
+    describe '#to_h' do
+      it { expect(RGBA.new(0, 0, 0, 0.0).to_h).to eq({ r: 0, g: 0, b: 0, a: 0.0 }) }
+      it { expect(RGBA.new(255, 255, 255, 1.0).to_h).to eq({ r: 255, g: 255, b: 255, a: 1.0 }) }
+    end
+
+    it 'raises ArgumentError for not Float value of alpha' do
+      expect {
+        RGBA.new(0, 0, 0, 0)
+      }.to raise_exception(
+             ArgumentError,
+             'Wrong alpha value 0 should be Float between 0.0 (fully transparent) and 1.0 (fully opaque)')
+    end
+
+    it 'raises ArgumentError wrong value of alpha' do
+      expect {
+        RGBA.new(0, 0, 0, 2.0)
+      }.to raise_exception(
+             ArgumentError,
+             'Wrong alpha value 2.0 should be Float between 0.0 (fully transparent) and 1.0 (fully opaque)')
+    end
+
+    it 'raises ArgumentError for wrong value of red' do
+      expect {
+        RGBA.new(-1, 0, 0, 0.0)
+      }.to raise_exception(ArgumentError, 'Wrong value of -1 should be Integer from 0 to 255')
+    end
+
+    it 'raises ArgumentError for wrong value of green' do
+      expect {
+        RGBA.new(0, 256, 0, 0.0)
+      }.to raise_exception(ArgumentError, 'Wrong value of 256 should be Integer from 0 to 255')
+    end
+
+    it 'raises ArgumentError for wrong value of blue' do
+      expect {
+        RGBA.new(0, 0, 0.0, 0.0)
+      }.to raise_exception(ArgumentError, 'Wrong value of 0.0 should be Integer from 0 to 255')
+    end
+  end
+end
