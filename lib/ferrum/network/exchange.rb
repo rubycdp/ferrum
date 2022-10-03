dd3ed84f1140040d99d867d01dedbff7a9577c76
Postmodern <postmodern.mod3@gmail.com>
2022-10-03 16:11:49 -0700

    Added YARD tags to `Ferrum::Network::Exchange`.

--- lib/ferrum/network/exchange.rb
+++ lib/ferrum/network/exchange.rb
@@ -4,8 +4,31 @@ module Ferrum
   class Network
     class Exchange
       attr_reader :id
-      attr_accessor :intercepted_request, :request, :response, :error
 
+      # The intercepted request.
+      #
+      # @return [InterceptedRequest, nil]
+      attr_accessor :intercepted_request
+
+      # The request object.
+      #
+      # @return [Request, nil]
+      attr_accessor :request
+
+      # The response object.
+      #
+      # @return [Response, nil]
+      attr_accessor :response
+
+      attr_accessor :error
+
+      #
+      # Initializes the network exchange.
+      #
+      # @param [Page] page
+      #
+      # @param [String] id
+      #
       def initialize(page, id)
         @id = id
         @page = page
@@ -13,35 +36,79 @@ module Ferrum
         @request = @response = @error = nil
       end
 
+      #
+      # Determines if the network exchange was caused by a page navigation
+      # event.
+      #
+      # @param [String] frame_id
+      #
+      # @return [Boolean]
+      #
       def navigation_request?(frame_id)
         request.type?(:document) &&
           request.frame_id == frame_id
       end
 
+      #
+      # Determines if the network exchange has a request.
+      #
+      # @return [Boolean]
+      #
       def blank?
         !request
       end
 
+      #
+      # Determines if the request was intercepted and blocked.
+      #
+      # @return [Boolean]
+      #
       def blocked?
         intercepted? && intercepted_request.status?(:aborted)
       end
 
+      #
+      # Determines if the request was blocked, a response was returned, or if an
+      # error occurred.
+      #
+      # @return [Boolean]
+      #
       def finished?
         blocked? || response || error
       end
 
+      #
+      # Determines if the network exchange is still not finished.
+      #
+      # @return [Boolean]
+      #
       def pending?
         !finished?
       end
 
+      #
+      # Determines if the exchange's request was intercepted.
+      #
+      # @return [Boolean]
+      #
       def intercepted?
         intercepted_request
       end
 
+      #
+      # Converts the network exchange into a request, response, and error tuple.
+      #
+      # @return [Array]
+      #
       def to_a
         [request, response, error]
       end
 
+      #
+      # Inspects the network exchange.
+      #
+      # @return [String]
+      #
       def inspect
         "#<#{self.class} " \
           "@id=#{@id.inspect} " \
