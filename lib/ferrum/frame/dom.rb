5384c0a86e91c7767b7c9cb6431e848c01fa6005
Alexander Brandon Coles <a.coles@openproject.com>
2025-02-23 16:09:12 -0300

    feat: Add Frame#frame_element method
    Faciliates easier retrieval of the `iframe` element that embeds the
    `window` (represented as a `Frame` object by Ferrum).

--- lib/ferrum/frame/dom.rb
+++ lib/ferrum/frame/dom.rb
@@ -96,6 +96,22 @@ module Ferrum
       end
 
       #
+      # Returns the element in which the window is embedded.
+      #
+      # @return [Node, nil]
+      #   The element in which the window is embedded.
+      #
+      # @example
+      #   browser.go_to("https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe")
+      #   frame = browser.frames.last
+      #   frame.frame_element # => [Node]
+      #   frame.parent.parent.parent.frame_element # => nil
+      #
+      def frame_element
+        evaluate("window.frameElement")
+      end
+
+      #
       # Finds nodes by using a XPath selector.
       #
       # @param [String] selector
